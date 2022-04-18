c4ecff76cb11c62a21f5d4acec78b208d6c38b85
mwangiwambui <wambui54mwangi@gmail.com>
2022-04-18 10:09:27 +0300

    Changed from class components to functional components

--- frontend/myportfolio/src/Components/Services.js
+++ frontend/myportfolio/src/Components/Services.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 
-export class Services extends Component {
-  render() {
+const Services = () => {
     return (
       <section class="services section-padding bg-dark position-re" data-scroll-index="2">
         <div class="container">
@@ -49,5 +48,6 @@ export class Services extends Component {
         <svg class="wave" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 1920 81.717" enable-background="new 0 0 1920 81.717" xml:space="preserve"><path style="" d="M1920,0c0,0-109.246,46.107-316.333,67.334C1343.5,94,1137.095,77.238,999.167,67.5C854,57.25,637.662,24.697,541.709,18.834C375.334,8.666,147,11,0,37.875V0H1920L1920,0z"></path></svg>
       </section>
     )
-  }
 }
+
+export default Services
